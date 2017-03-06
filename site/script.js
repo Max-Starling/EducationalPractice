d18b8c21c213763824e9ce4c15bbe3a379ac7804
Max Starling <17.max.starling@gmail.com>
2017-03-07 00:22:26 +0300

    "script.js" was added.

---
+++ site/script.js
@@ -0,0 +1,362 @@
+/**
+ * Created by Max-Starling on 05.03.2017.
+ */
+var news = [
+    /*var new1 = */{
+        ID: '0001',
+        title: 'CHAPTER I',
+        summary: 'Down the Rabbit-Hole',
+        createdAt: new Date('2017-03-05T00:00:00'),
+        author: 'Lewis Carroll',
+        content: 'Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: ' +
+        'once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, ' +
+        'and what is the use of a book, thought Alice without pictures or conversation?'
+    },
+    /*var new2 = */{
+        ID: '0002',
+        title: 'CHAPTER II',
+        summary: 'The Pool of Tears',
+        createdAt: new Date("2017-03-05T01:01:01"),
+        author: 'Lewis Carroll',
+        content: 'Curiouser and curiouser! cried Alice (she was so much surprised, that for the moment she quite forgot how ' +
+        'to speak good English); now I am opening out like the largest telescope that ever was!'
+    },
+    /*var new3 = */{
+        ID: '0003',
+        title: 'CHAPTER III',
+        summary: 'A Caucus-Race and a Long Tale',
+        createdAt: new Date("2017-03-05T02:02:02"),
+        author: 'Lewis Carroll',
+        content: 'They were indeed a queer-looking party that assembled on the bank—the birds with draggled feathers, the ' +
+        'animals with their fur clinging close to them, and all dripping wet, cross, and uncomfortable.'
+    },
+    /*var new4 = */{
+        ID: '0004',
+        title: 'CHAPTER IV',
+        summary: 'The Rabbit Sends in a Little Bill',
+        createdAt: new Date("2017-03-05T03:03:03"),
+        author: 'Lewis Carroll',
+        content: 'It was the White Rabbit, trotting slowly back again, and looking anxiously about as it went, as if it had ' +
+        'lost something; and she heard it muttering to itself.'
+    },
+    /*var new5 = */{
+        ID: '0005',
+        title: 'CHAPTER V',
+        summary: 'Advice from a Caterpillar',
+        createdAt: new Date("2017-03-05T05:05:05"),
+        author: 'Lewis Carroll',
+        content: 'The Caterpillar and Alice looked at each other for some time in silence: at last the Caterpillar took the ' +
+        'hookah out of its mouth, and addressed her in a languid, sleepy voice.'
+    },
+    /*var new6 = */{
+        ID: '0006',
+        title: 'CHAPTER VI',
+        summary: 'Pig and Pepper',
+        createdAt: new Date("2017-03-05T06:06:06"),
+        author: 'Lewis Carroll',
+        content: 'The Caterpillar and Alice looked at each other for some time in silence: at last the Caterpillar took the ' +
+        'hookah out of its mouth, and addressed her in a languid, sleepy voice.For a minute or two she stood looking at the ' +
+        'house, and wondering what to do next, when suddenly a footman in livery came running out of the wood—(she considered ' +
+        'him to be a footman because he was in livery: otherwise, judging by his face only, she would have called him a fish)' +
+        '—and rapped loudly at the door with his knuckles.'
+    },
+    /*var new7 = */{
+        ID: '0007',
+        title: 'CHAPTER VII',
+        summary: 'A Mad Tea-Party',
+        createdAt: new Date("2017-03-05T07:07:07"),
+        author: 'Lewis Carroll',
+        content: 'There was a table set out under a tree in front of the house, and the March Hare and the Hatter were having ' +
+        'tea at it: a Dormouse was sitting between them, fast asleep, and the other two were using it as a cushion, resting their ' +
+        'elbows on it, and talking over its head.'
+    },
+    /*var new8 = */{
+        ID: '0008',
+        title: 'CHAPTER VIII',
+        summary: 'The Queens Croquet-Ground',
+        createdAt: new Date("2017-03-05T08:08:08"),
+        author: 'Lewis Carroll',
+        content: 'A large rose-tree stood near the entrance of the garden: the roses growing on it were white, but there were ' +
+        'three gardeners at it, busily painting them red.'
+    },
+    /*var new9 = */{
+        ID: '0009',
+        title: 'CHAPTER IX',
+        summary: 'The Mock Turtles Story',
+        createdAt: new Date("2017-03-05T09:09:09"),
+        author: 'Lewis Carroll',
+        content: 'You cant think how glad I am to see you again, you dear old thing! said the Duchess, as she tucked her arm ' +
+        'affectionately into Alices, and they walked off together.'
+    },
+    /*var new10 = */{
+        ID: '0010',
+        title: 'CHAPTER X',
+        summary: 'The Lobster Quadrille',
+        createdAt: new Date("2017-03-05T10:10:10"),
+        author: 'Lewis Carroll',
+        content: 'The Mock Turtle sighed deeply, and drew the back of one flapper across his eyes. He looked at Alice, and tried ' +
+        'to speak, but for a minute or two sobs choked his voice.'
+    },
+    /*var new11 = */{
+        ID: '0011',
+        title: 'CHAPTER XI',
+        summary: 'Who Stole the Tarts?',
+        createdAt: new Date("2017-03-05T11:11:11"),
+        author: 'Lewis Carroll',
+        content: 'The King and Queen of Hearts were seated on their throne when they arrived, with a great crowd assembled about ' +
+        'them—all sorts of little birds and beasts, as well as the whole pack of cards: the Knave was standing before them, in ' +
+        'chains, with a soldier on each side to guard him; and near the King was the White Rabbit, with a trumpet in one hand, ' +
+        'and a scroll of parchment in the other.'
+    },
+    /*var new12 = */{
+        ID: '0012',
+        title: 'CHAPTER XII',
+        summary: 'Alices Evidence',
+        createdAt: new Date("2017-03-05T12:12:12"),
+        author: 'Lewis Carroll',
+        content: 'Here! cried Alice, quite forgetting in the flurry of the moment how large she had grown in the last few minutes, ' +
+        'and she jumped up in such a hurry that she tipped over the jury-box with the edge of her skirt, upsetting all the jurymen ' +
+        'on to the heads of the crowd below, and there they lay sprawling about, reminding her very much of a globe of goldfish ' +
+        'she had accidentally upset the week before.'
+    },
+    /*var new13 = */{
+        ID: '0013',
+        title: 'I',
+        summary: 'Once',
+        createdAt: new Date("2017-03-05T13:13:13"),
+        author: 'Antoine de Saint-Exupery',
+        content: 'Once when I was six years old I saw a magnificent picture in a book, called True Stories from Nature, about ' +
+        'the primeval forest. '
+    },
+    /*var new14 = */{
+        ID: '0014',
+        title: 'II',
+        summary: 'So',
+        createdAt: new Date("2017-03-05T14:14:14"),
+        author: 'Antoine de Saint-Exupery',
+        content: 'So I lived my life alone, without anyone that I could really talk to, until I had an accident with my plane ' +
+        'in the Desert of Sahara, six years ago.  '
+    },
+    /*var new15 = */{
+        ID: '0015',
+        title: 'III',
+        summary: 'It',
+        createdAt: new Date("2017-03-05T15:15:15"),
+        author: 'Antoine de Saint-Exupery',
+        content: 'It took me a long time to learn where he came from. The little prince, who asked me so many questions, never ' +
+        'seemed to hear the ones I asked him. '
+    },
+    /*var new16 = */{
+        ID: '0016',
+        title: 'IV',
+        summary: 'I',
+        createdAt: new Date("2017-03-05T16:16:16"),
+        author: 'Antoine de Saint-Exupery',
+        content: 'I had thus learned a second fact of great importance: this was that the planet the little prince came from ' +
+        'was scarcely any larger than a house!'
+    },
+    /*var new17 = */{
+        ID: '0017',
+        title: 'V',
+        summary: 'As',
+        createdAt: new Date("2017-03-05T17:17:17"),
+        author: 'Antoine de Saint-Exupery',
+        content: 'As each day passed I would learn, in our talk, something about the little princes planet, his departure from ' +
+        'it, his journey. '
+    },
+    /*var new18 = */{
+        ID: '0018',
+        title: 'VI',
+        summary: 'Oh',
+        createdAt: new Date("2017-03-05T18:18:18"),
+        author: 'Antoine de Saint-Exupery',
+        content: 'Oh, little prince! Bit by bit I came to understand the secrets of your sad little life...'
+    },
+    /*var new19 = */{
+        ID: '0019',
+        title: 'VII',
+        summary: 'On',
+        createdAt: new Date("2017-03-05T19:19:19"),
+        author: 'Antoine de Saint-Exupery',
+        content: 'On the fifth day—again, as always, it was thanks to the sheep—the secret of the little princes life was ' +
+        'revealed to me.'
+    },
+    /*var new20 = */{
+        ID: '0020',
+        title: 'VIII',
+        summary: 'I',
+        createdAt: new Date("2017-03-05T20:20:20"),
+        author: 'Antoine de Saint-Exupery',
+        content: 'I soon learned to know this flower better. On the little princes planet the flowers had always been very simple. ' +
+        'They had only one ring of petals; they took up no room at all; they were a trouble to nobody. '
+    }
+];
+function validateNew(n) {
+    /*ID*/
+    if (!n.ID) {
+        console.log("Oops. Something is wrong with author. You should try again later.");
+        return false;
+    }
+    /*Title*/
+    if (!n.title){
+        console.log("Oops. Something is wrong with author. You should try again later.");
+        return false;
+    }else{
+        if ((n.title.length >= 100) || n.title.length == 0){
+            console.log("Oops. Something is wrong with title length. You should check it and try again.");
+            return false;
+        }
+    }
+    /*Summary*/
+    if(!n.summary){
+        return false;
+    }else if(n.summary.length >= 300 ||  n.summary.length == 0){
+            console.log("Oops. Something is wrong with summary length. You should check it and try again.");
+            return false;
+    }
+    /*Date*/
+    if (!n.createdAt)
+    {
+        console.log("Oops. Something is wrong with date. You should try again later.");
+        return false;
+    }
+    /*Author*/
+    if(!n.author) {
+        console.log("Oops. Something is wrong with author. You should try again later.");
+        return false;
+    }else if (n.author.length == 0){
+        console.log("Oops. Something is wrong with author name length. You should check it and try again.");
+        return false;
+    }
+    /*Content*/
+    if(!n.content){
+        console.log("Oops. Something is wrong with content. You should check it and try again.");
+        return false;
+    }else if (n.content.length == 0){
+        console.log("Oops. Something is wrong with content length. You should check it and try again.");
+        return false;
+    }
+    return true;
+}
+
+function getNew(ID) {
+    for (i = 0; i < news.length; i++) {
+        if (news[i].ID == ID) {
+            return news[i];
+        }
+    }
+    return undefined;
+}
+function sortNews(few){
+    few.sort(
+        function (x, y){
+            if (x.createdAt < y.createdAt){
+                return 1;
+            }
+            if (x.createdAt > y.createdAt){
+                return -1;
+            }
+        }
+    );
+}
+
+
+function getNews(skip, top, filterConfig) {
+    /*Default*/
+    if (skip === undefined) {
+        skip = 0;
+    }
+    if (top === undefined) {
+        top = 10;
+    }
+    var out = news.slice();
+    sortNews(out);
+    if (filterConfig) {
+        /*Author*/
+        if (filterConfig.author) {
+            out = out.filter(function (n) {
+                return filterConfig.author === n.author;
+            });
+        }
+        /*Date*/
+        if (filterConfig.createdAt) {
+            filterConfig.createdAt = new Date(filterConfig.createdAt);
+            out = out.filter(
+                function (n)
+                {
+                    return filterConfig.createdAt === n.createdAt;
+                }
+            );
+        }
+    }
+    return out.slice(skip, skip + top);
+}
+var  next = 21;
+addNew(
+    {
+        id: '00' + next.toString(),
+        title: "Title",
+        summary: "Summary",
+        createdAt: new Date('1970-01-01T00:00'),
+        author: "You",
+        content: "Content"
+    }
+);
+
+function addNew(n) {
+    if (validateNew(n)){
+        news.push(n);
+        next++;
+    } else{
+        return false;
+    }
+}
+
+function removeNew(ID) {
+    var tmp = 0;
+    for(var i = 0; i < news.length; i++){
+        if(news[i].ID == ID) {
+            tmp = i;
+        }
+    }
+    news.splice(index, 1);
+}
+
+news.forEach(
+    function (n){
+        console.log(validateNew(n));
+    }
+);
+function editNew(ID, n) {
+    var i = 0;
+    for (; news.articles[i].ID != ID; i++) {
+        if (i + 1 >= news.length) {
+            return false;
+        }
+    }
+    if (news[i] !== undefined){
+        var tmp = news[i];
+        if (n.title && n.title.length < 100){
+            news[i].title = n.title;
+        }
+        if (n.summary && n.summary.length < 200){
+            news[i].summary = n.summary;
+        }
+        if (n.createdAt){
+            news[i].createdAt = n.createdAt;
+        }
+        if (n.author){
+            news[i].author = n.author;
+        }
+        if (article.content){
+            articles[i].content = article.content;
+        }
+        if (!validateNew(news[i])){
+            articles[i] = tmp;
+            return false;
+        }else{
+            return true;
+        }
+    }
+    return false;
+}
