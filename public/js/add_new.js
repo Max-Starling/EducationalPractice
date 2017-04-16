533e3e82e0c6cff1299a266605cece2e19645e8c
Max Starling <17.max.starling@gmail.com>
2017-04-16 03:19:45 +0300

    refactoring.

--- public/js/add_new.js
+++ public/js/add_new.js
@@ -1,102 +1,107 @@
-function addNew(){
-		var overlay = document.querySelector('.modal-overlay');
-        var modalContent = document.getElementsByClassName("modal-content1")[0]; 
-        var form = document.getElementsByClassName("add-new-form")[0];
-        var inputURL = document.getElementsByClassName("add-new-input")[0];
-        document.getElementsByClassName("modal-title")[0].textContent = "ADD NEW";
-        inputURL.placeholder = "Image URL";
-        inputURL.type = "text";
-        inputURL.value = "";
-        var inputTitle = document.getElementsByClassName("add-new-input")[1];
-        inputTitle.style.marginTop = "0.5vw"     
-        inputTitle.placeholder = "Title";
-        inputTitle.type = "text";
-        inputTitle.maxLength = "24";
-        inputTitle.value = "";
-        var inputShortDescription = document.getElementsByClassName("add-new-textarea")[0];
-        inputShortDescription.style.marginTop = "0.5vw"
-        inputShortDescription.maxLength = "80";
-        inputShortDescription.value = "";
-        var inputContent = document.getElementsByClassName("add-new-textarea")[1];
-        inputContent.style.height = "9.6vw";
-        inputContent.style.marginTop = "0.5vw";
-        inputContent.maxLength = "1280";
-        inputContent.value = "";
-        form.spellcheck = false;
-        form.onsubmit = function(event){
-            event.preventDefault();
-        };        
-        [].slice.call(document.querySelectorAll('.modal-trigger1')).forEach( 				
-            function(el, i){
-                var modal = document.querySelector('#' + el.getAttribute('data-modal'));
-                    function removeModalHandler(){
-                        classie.remove(modal,'modal-show1');
-                    }
-                    el.addEventListener('click', 
-                        function(){
-                            classie.add(modal, 'modal-show1');
-                            overlay.removeEventListener('click', removeModalHandler);
-                            overlay.addEventListener('click', removeModalHandler);
-                        }
- 		            );
-                    var close = modal.querySelector( '.md-close' );
-					close.addEventListener('click', function(ev){
-						ev.stopPropagation();
-                        var n = {
-                            ID: (newModel.getLength() + 1) + '',
-                            title: "",
-                            summary: "",
-                            createdAt: new Date(),
-                            author: "You",
-                            content: "",
-                            img: ""
-                        }
-                        var ID = newModel.getLength() + 1;
-                        var correctID = "";
-                        if(ID >= 10){
-                            correctID = "00" + ID;
-                        }else if(ID >= 100){
-                            correctID = "0" + ID;
-                        }else if(ID >= 1000){
-                            correctID = ID;
-                        }else{
-                            correcID = "000" + ID;
-                        }
-                        n.img = inputURL.value.toString();
-                        n.title = inputTitle.value.toString();
-                        n.summary = inputShortDescription.value.toString();
-                        n.content = inputContent.value.toString();
-                        newModel.addNew(n);
-                        newRenderer.removeNewsFromDom();
-                        var news = newModel.getNews(0, newModel.getLength());
-                        newRenderer.insertNewsInDOM(news);
-                        event.stopImmediatePropagation();
-                        if(!n.title){
-                              inputTitle.style.color = "#8b1500";
-                        }else{
-                            inputTitle.style.color = "#aaaaaa";
-                        }
-                        if(!n.content){
-                            inputContent.style.color = "#8b1500";
-                        }else{
-                            inputContent.style.color = "#aaaaaa";
-                        }
-                        if(!n.summary){
-                            inputShortDescription.style.color = "#8b1500";
-                        }else{
-                            inputShortDescription.style.color = "#aaaaaa";
-                        }
-                        if(n.title && n.summary && n.content){
-                            inputURL.value = "";
-                            inputContent.value = "";
-                            inputShortDescription.value = "";
-                            inputTitle.value = "";
-                            inputTitle.style.color = "#aaaaaa";
-                            inputContent.style.color = "#aaaaaa";
-                            inputShortDescription.style.color = "#aaaaaa";
-						    removeModalHandler();
-                        }
-					});
+function addNew() {
+    var overlay = document.querySelector('.modal-overlay');
+    var modalContent = document.querySelector('.modal-content1');
+    var form = modalContent.querySelector('.add-new-form');
+    modalContent.querySelector('.modal-title').textContent = "ADD NEW";
+
+    var inputURL = form.querySelectorAll('.add-new-input')[0];
+    inputURL.placeholder = "Image URL";
+    inputURL.type = "text";
+    inputURL.value = "";
+
+    var inputTitle = form.querySelectorAll('.add-new-input')[1];
+    inputTitle.style.marginTop = "0.5vw"
+    inputTitle.placeholder = "Title";
+    inputTitle.type = "text";
+    inputTitle.maxLength = "24";
+    inputTitle.value = "";
+
+    var inputShortDescription = form.querySelectorAll('.add-new-textarea')[0];
+    inputShortDescription.style.marginTop = "0.5vw"
+    inputShortDescription.maxLength = "80";
+    inputShortDescription.value = "";
+
+    var inputContent = form.querySelectorAll('.add-new-textarea')[1];
+    inputContent.style.height = "9.6vw";
+    inputContent.style.marginTop = "0.5vw";
+    inputContent.maxLength = "1280";
+    inputContent.value = "";
+
+    form.spellcheck = false;
+    form.onsubmit = function (event) {
+        event.preventDefault();
+    };
+
+    var el = document.querySelector('.modal-trigger1');
+    var modal = document.querySelector('#' + el.getAttribute('data-modal'));
+    function removeModalHandler() {
+        classie.remove(modal, 'modal-show1');
+    }
+    el.addEventListener('click',
+        function () {
+            classie.add(modal, 'modal-show1');
+            overlay.removeEventListener('click', removeModalHandler);
+            overlay.addEventListener('click', removeModalHandler);
+        }
+    );
+    var close = modal.querySelector('.md-close');
+    close.addEventListener('click',
+        function (ev) {
+            ev.stopPropagation();
+            var n = {
+                ID: (newModel.getLength() + 1) + '',
+                title: "",
+                summary: "",
+                createdAt: new Date(),
+                author: "You",
+                content: "",
+                img: ""
             }
-	    );
+            var ID = newModel.getLength() + 1;
+            var correctID = "";
+            if (ID >= 10) {
+                correctID = "00" + ID;
+            } else if (ID >= 100) {
+                correctID = "0" + ID;
+            } else if (ID >= 1000) {
+                correctID = ID;
+            } else {
+                correcID = "000" + ID;
+            }
+            n.img = inputURL.value.toString();
+            n.title = inputTitle.value.toString();
+            n.summary = inputShortDescription.value.toString();
+            n.content = inputContent.value.toString();
+            newModel.addNew(n);
+            newRenderer.removeNewsFromDom();
+            var news = newModel.getNews(0, newModel.getLength());
+            newRenderer.insertNewsInDOM(news);
+            event.stopImmediatePropagation();
+            if (!n.title) {
+                inputTitle.style.color = "#8b1500";
+            } else {
+                inputTitle.style.color = "#aaaaaa";
+            }
+            if (!n.content) {
+                inputContent.style.color = "#8b1500";
+            } else {
+                inputContent.style.color = "#aaaaaa";
+            }
+            if (!n.summary) {
+                inputShortDescription.style.color = "#8b1500";
+            } else {
+                inputShortDescription.style.color = "#aaaaaa";
+            }
+            if (n.title && n.summary && n.content) {
+                inputURL.value = "";
+                inputContent.value = "";
+                inputShortDescription.value = "";
+                inputTitle.value = "";
+                inputTitle.style.color = "#aaaaaa";
+                inputContent.style.color = "#aaaaaa";
+                inputShortDescription.style.color = "#aaaaaa";
+                removeModalHandler();
+            }
+        });
+//);
 }
--- public/js/settings.js
+++ public/js/settings.js
@@ -1,38 +1,36 @@
 function settings(){
-		const overlay = document.querySelector('.modal-overlay');
-        const modalContent = document.getElementsByClassName("modal-content5")[0]; 
-        document.getElementsByClassName("modal-title")[4].textContent = "SETTINGS";
-        //  Form  //
-        const form = document.getElementsByClassName("settings-form")[0];
-        form.spellcheck = false;
-        form.onsubmit = function(event){
-            event.preventDefault(); 
-        };
+	const overlay = document.querySelector('.modal-overlay');
+    const modalContent = document.querySelector('.modal-content5'); 
+    modalContent.querySelector('.modal-title').textContent = "SETTINGS";
+    //  Form  //
+    const form = document.querySelector('.settings-form');
+    form.spellcheck = false;
+    form.onsubmit = function(event){
+        event.preventDefault(); 
+    };
 
-        const slider = document.getElementsByClassName("slider")[0];
-        //alert(slider.value);
-        [].slice.call(document.querySelectorAll('.modal-trigger6')).forEach(		
-            function(el, i){
-                const modal = document.querySelector('#' + el.getAttribute('data-modal'));
-                    function removeModalHandler(){
-                        classie.remove(modal,'modal-show5');
-                    }
-                    el.addEventListener('click', 
-                        function(){
-                            classie.add(modal, 'modal-show5');
-                            overlay.removeEventListener('click', removeModalHandler);
-                            overlay.addEventListener('click', removeModalHandler);
-                        }
- 		            );
-                    const close = modal.querySelector('.md-close');
-					close.addEventListener('click', function(ev){
-                        //  Saturation effect from 0.5 to 1.5 with slider  //
-                        let value = (slider.value / 100) + 0.5;
-                        const wrapper = document.getElementsByClassName("wrapper")[0];
-                        wrapper.style.filter = "saturate(" + value + ")";
-						ev.stopPropagation();
-                        removeModalHandler();
-				});
-            }
-	    );
+    const slider = document.querySelector('.slider');
+    var el = document.querySelector('.modal-trigger6');  
+    const modal = document.querySelector('#' + el.getAttribute('data-modal'));
+    function removeModalHandler(){
+        classie.remove(modal,'modal-show5');
+    }
+    el.addEventListener('click', 
+        function(){
+            classie.add(modal, 'modal-show5');
+            overlay.removeEventListener('click', removeModalHandler);
+            overlay.addEventListener('click', removeModalHandler);
+        }
+ 	);
+    const close = modal.querySelector('.md-close');
+	close.addEventListener('click', 
+        function(event){
+            //  Saturation effect from 0.5 to 1.5 with slider  //
+            let value = (slider.value / 100) + 0.5;
+            const wrapper = document.getElementsByClassName("wrapper")[0];
+            wrapper.style.filter = "saturate(" + value + ")";
+            event.stopPropagation();
+            removeModalHandler();
+        }
+    );      
 }
