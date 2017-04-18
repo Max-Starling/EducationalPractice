bbddfc441d8403ddc46e723398f4e39cb9767c63
Max Starling <17.max.starling@gmail.com>
2017-04-18 13:28:43 +0300

    restoring git.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
+
+
 app.use(express.static('public'));
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-});
+app.listen(7777, 
+    function () {
+        console.log('STARLING NEWS listening on port 7777! Link:"http://localhost:7777"');
+    }
+);
